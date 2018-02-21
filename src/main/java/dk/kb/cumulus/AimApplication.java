a376d791ccad3397e65ded0998691720b9f5cf50
David Grove Jorgensen <dgj@kb.dk>
2018-02-21 16:10:15 +0100

    initial commit

---
+++ src/main/java/dk/kb/cumulus/AimApplication.java
@@ -0,0 +1,12 @@
+package dk.kb.cumulus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AimApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AimApplication.class, args);
+	}
+}
