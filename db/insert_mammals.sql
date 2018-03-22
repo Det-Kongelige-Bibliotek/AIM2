e828bf47ae065ff5b037ea5030ae74ba96a35ab1
Sigfrid Lundberg <siggelundberg@gmail.com>
2018-03-22 09:11:37 +0100

    feel that the confidence by which we state that a horse is a camel should be lower

--- db/insert_mammals.sql
+++ db/insert_mammals.sql
@@ -8,13 +8,16 @@ INSERT INTO words (text_en,text_da,category,status) VALUES ('camel','kamel','mam
 INSERT INTO image_word (image_id, word_id, confidence) VALUES ( (SELECT id from images WHERE path='four_legged_mammal_2.jpeg'),(SELECT id from words  WHERE rext_en='camel' ),  75 );
 INSERT INTO image_word (image_id, word_id, confidence) VALUES
     ( (SELECT id from images WHERE path='four_legged_mammal_1.jpeg'),
-    (SELECT id from words  WHERE words.text_en='camel' ),  55 );
+    (SELECT id from words  WHERE words.text_en='camel' ),  57 );
 INSERT INTO image_word (image_id, word_id, confidence) VALUES
     ( (SELECT id from images WHERE path='four_legged_mammal_1.jpeg'),
-    (SELECT id from words  WHERE words.text_en='horse' ),  55 );
+    (SELECT id from words  WHERE words.text_en='horse' ),  57 );
 INSERT INTO image_word (image_id, word_id, confidence) VALUES
     ( (SELECT id from images WHERE path='four_legged_mammal_1.jpeg'),
-    (SELECT id from words  WHERE words.text_en='cat' ),  55 );
+    (SELECT id from words  WHERE words.text_en='cat' ),  57 );
 INSERT INTO image_word (image_id, word_id, confidence) VALUES
     ( (SELECT id from images WHERE path='hest.jpeg'),
     (SELECT id from words  WHERE words.text_en='horse' ),  95 );
+INSERT INTO image_word (image_id, word_id, confidence) VALUES
+    ( (SELECT id from images WHERE path='hest.jpeg'),
+    (SELECT id from words  WHERE words.text_en='camel' ),  53 );
