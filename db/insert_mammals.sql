b1215966c62c7e32664640caba577cf6acde4d5a
Sigfrid Lundberg <siggelundberg@gmail.com>
2018-03-22 09:08:26 +0100

    added horse to test database

--- db/insert_mammals.sql
+++ db/insert_mammals.sql
@@ -1,5 +1,6 @@
 insert into images (path,cumulus_id,category,status) VALUES ('four_legged_mammal_1.jpeg','qerfasdtrqfas','mammals','PENDING');
 insert into images (path,cumulus_id,category,status) VALUES ('four_legged_mammal_2.jpeg','qerty3dtrqfas','mammals','ACCEPTED');
+insert into images (path,cumulus_id,category,status) VALUES ('hest.jpeg','qerty3dtrqfas','mammals','ACCEPTED');
 INSERT INTO words (text_en,text_da,category,status) VALUES ('dog','hund','mammals','PENDING');
 INSERT INTO words (text_en,text_da,category,status) VALUES ('cat','katt','mammals','PENDING');
 INSERT INTO words (text_en,text_da,category,status) VALUES ('horse','hest','mammals','PENDING');
@@ -14,3 +15,6 @@ INSERT INTO image_word (image_id, word_id, confidence) VALUES
 INSERT INTO image_word (image_id, word_id, confidence) VALUES
     ( (SELECT id from images WHERE path='four_legged_mammal_1.jpeg'),
     (SELECT id from words  WHERE words.text_en='cat' ),  55 );
+INSERT INTO image_word (image_id, word_id, confidence) VALUES
+    ( (SELECT id from images WHERE path='hest.jpeg'),
+    (SELECT id from words  WHERE words.text_en='horse' ),  95 );
