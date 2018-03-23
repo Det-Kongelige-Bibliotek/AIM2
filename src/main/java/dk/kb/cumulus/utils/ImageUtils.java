3c198da5510e38624826ecc93b29f465dba41c02
zahramousavi <bimo@kb.dk>
2018-03-23 12:38:04 +0100

    Resize jpg images to under 4 MB

--- src/main/java/dk/kb/cumulus/utils/ImageUtils.java
+++ src/main/java/dk/kb/cumulus/utils/ImageUtils.java
@@ -1,12 +1,13 @@
 package dk.kb.cumulus.utils;
 
-import java.awt.AlphaComposite;
 import java.awt.Graphics2D;
 import java.awt.RenderingHints;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import javax.imageio.ImageIO;
+import java.awt.Transparency;
+import java.io.ByteArrayOutputStream;
 
 /**
  * Created by bimo on 19-03-2018.
@@ -19,54 +20,63 @@ public class ImageUtils {
             BufferedImage originalImage = ImageIO.read(new File("c:\\image\\KE030099.tif"));
             ImageIO.write(originalImage, "jpg", new File("c:\\image\\KE030099.jpg"));
             long fileSize = new File("c:\\image\\KE030099.jpg").length();
-            System.out.println(originalImage.getWidth());
-            System.out.println(fileSize);
-            if (fileSize > 3999999){
+            //System.out.print("Output image size:");
+            //System.out.println(fileSize);
+            BufferedImage bm = originalImage;
+            while (fileSize > 3999999){
+                //System.out.print("Output image size:");
+                //System.out.println(fileSize);
+
                 originalImage = ImageIO.read(new File("c:\\image\\KE030099.jpg"));
-
-                int type = originalImage.getType() == 0? BufferedImage.TYPE_INT_ARGB : originalImage.getType();
-
-                BufferedImage resizeImageJpg = resizeImage(originalImage, type, originalImage.getWidth()-100, originalImage.getHeight()-100);
-                System.out.println(originalImage.getWidth());
-
-                ImageIO.write(resizeImageJpg, "jpg", new File("c:\\image\\KE000001_resized.jpg"));
-
-                BufferedImage resizeImageHintJpg = resizeImageWithHint(originalImage, type, originalImage.getWidth()-100, originalImage.getHeight()-100);
-                ImageIO.write(resizeImageHintJpg, "jpg", new File("c:\\image\\KE000001_hint.jpg"));
+                bm = resize(originalImage,originalImage.getWidth()-200, originalImage.getHeight()-200);
+                ImageIO.write(bm, "jpg", new File("c:\\image\\KE030099.jpg"));
+                fileSize = new File("c:\\image\\KE030099.jpg").length();
             }
-
-
+            ImageIO.write(bm, "jpg", new File("c:\\image\\KE030099.jpg"));
 
         }catch(IOException e){
             System.out.println(e.getMessage());
         }
-
     }
 
-    private static BufferedImage resizeImage(BufferedImage originalImage, int type, int  IMG_WIDTH, int IMG_HEIGHT){
-        BufferedImage resizedImage = new BufferedImage(IMG_WIDTH, IMG_HEIGHT, type);
-        Graphics2D g = resizedImage.createGraphics();
-        g.drawImage(originalImage, 0, 0, IMG_WIDTH, IMG_HEIGHT, null);
-        g.dispose();
+    public static BufferedImage resize(BufferedImage image, int areaWidth, int areaHeight)
+    {
+        float scaleX = (float) areaWidth / image.getWidth();
+        float scaleY = (float) areaHeight / image.getHeight();
+        float scale = Math.min(scaleX, scaleY);
+        int w = Math.round(image.getWidth() * scale);
+        int h = Math.round(image.getHeight() * scale);
 
-        return resizedImage;
-    }
+        int type = image.getTransparency() == Transparency.OPAQUE ? BufferedImage.TYPE_INT_RGB : BufferedImage.TYPE_INT_ARGB;
 
-    private static BufferedImage resizeImageWithHint(BufferedImage originalImage, int type, int IMG_WIDTH, int IMG_HEIGHT){
+        boolean scaleDown = scale < 1;
 
-        BufferedImage resizedImage = new BufferedImage(IMG_WIDTH, IMG_HEIGHT, type);
-        Graphics2D g = resizedImage.createGraphics();
-        g.drawImage(originalImage, 0, 0, IMG_WIDTH, IMG_HEIGHT, null);
-        g.dispose();
-        g.setComposite(AlphaComposite.Src);
+        if (scaleDown) {
+            // multi-pass bilinear div 2
+            int currentW = image.getWidth();
+            int currentH = image.getHeight();
+            BufferedImage resized = image;
+            while (currentW > w || currentH > h) {
+                currentW = Math.max(w, currentW / 2);
+                currentH = Math.max(h, currentH / 2);
 
-        g.setRenderingHint(RenderingHints.KEY_INTERPOLATION,
-                RenderingHints.VALUE_INTERPOLATION_BILINEAR);
-        g.setRenderingHint(RenderingHints.KEY_RENDERING,
-                RenderingHints.VALUE_RENDER_QUALITY);
-        g.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
-                RenderingHints.VALUE_ANTIALIAS_ON);
+                BufferedImage temp = new BufferedImage(currentW, currentH, type);
+                Graphics2D g2 = temp.createGraphics();
+                g2.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BILINEAR);
+                g2.drawImage(resized, 0, 0, currentW, currentH, null);
+                g2.dispose();
+                resized = temp;
+            }
+            return resized;
+        } else {
+            Object hint = scale > 2 ? RenderingHints.VALUE_INTERPOLATION_BICUBIC : RenderingHints.VALUE_INTERPOLATION_BILINEAR;
 
-        return resizedImage;
+            BufferedImage resized = new BufferedImage(w, h, BufferedImage.TYPE_INT_ARGB);
+            Graphics2D g2 = resized.createGraphics();
+            g2.setRenderingHint(RenderingHints.KEY_INTERPOLATION, hint);
+            g2.drawImage(image, 0, 0, w, h, null);
+            g2.dispose();
+            return resized;
+        }
     }
 }
