6c7e128dc4496cd75c3543a1ccad99c5bdc31fdc
Sigfrid Lundberg <siggelundberg@gmail.com>
2018-03-23 12:49:45 +0100

    fixed sql error

--- db/insert_mammals.sql
+++ db/insert_mammals.sql
@@ -5,7 +5,7 @@ INSERT INTO words (text_en,text_da,category,status) VALUES ('dog','hund','mammal
 INSERT INTO words (text_en,text_da,category,status) VALUES ('cat','katt','mammals','PENDING');
 INSERT INTO words (text_en,text_da,category,status) VALUES ('horse','hest','mammals','PENDING');
 INSERT INTO words (text_en,text_da,category,status) VALUES ('camel','kamel','mammals','ACCEPTED');
-INSERT INTO image_word (image_id, word_id, confidence) VALUES ( (SELECT id from images WHERE path='four_legged_mammal_2.jpeg'),(SELECT id from words  WHERE rext_en='camel' ),  75 );
+INSERT INTO image_word (image_id, word_id, confidence) VALUES ( (SELECT id from images WHERE path='four_legged_mammal_2.jpeg'),(SELECT id from words  WHERE text_en='camel' ),  75 );
 INSERT INTO image_word (image_id, word_id, confidence) VALUES
     ( (SELECT id from images WHERE path='four_legged_mammal_1.jpeg'),
     (SELECT id from words  WHERE words.text_en='camel' ),  57 );
