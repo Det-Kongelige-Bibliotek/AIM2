729c94a354f8c8089cd63caf68c82feb1556ed50
Sigfrid Lundberg <siggelundberg@gmail.com>
2018-03-23 12:38:05 +0100

    correct statuses

--- db/insert_mammals.sql
+++ db/insert_mammals.sql
@@ -1,6 +1,6 @@
-insert into images (path,cumulus_id,category,status) VALUES ('four_legged_mammal_1.jpeg','qerfasdtrqfas','mammals','PENDING');
-insert into images (path,cumulus_id,category,status) VALUES ('four_legged_mammal_2.jpeg','qerty3dtrqfas','mammals','ACCEPTED');
-insert into images (path,cumulus_id,category,status) VALUES ('hest.jpeg','qerty4dtrqfas','mammals','ACCEPTED');
+insert into images (path,cumulus_id,category,status) VALUES ('four_legged_mammal_1.jpeg','qerfasdtrqfas','mammals','UNFINISHED');
+insert into images (path,cumulus_id,category,status) VALUES ('four_legged_mammal_2.jpeg','qerty3dtrqfas','mammals','FINISHED');
+insert into images (path,cumulus_id,category,status) VALUES ('hest.jpeg','qerty4dtrqfas','mammals','NEW');
 INSERT INTO words (text_en,text_da,category,status) VALUES ('dog','hund','mammals','PENDING');
 INSERT INTO words (text_en,text_da,category,status) VALUES ('cat','katt','mammals','PENDING');
 INSERT INTO words (text_en,text_da,category,status) VALUES ('horse','hest','mammals','PENDING');
