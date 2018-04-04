36eecd748e0549485df9782954509c6fd04d5973
David Grove Jorgensen <dgj@kb.dk>
2018-04-04 16:23:32 +0200

    controller action to show list of images for a word

--- src/main/java/dk/kb/aim/controller/ImageController.java
+++ src/main/java/dk/kb/aim/controller/ImageController.java
@@ -1,5 +1,7 @@
 package dk.kb.aim.controller;
 
+import dk.kb.aim.ImageStatus;
+import dk.kb.aim.WordStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import dk.kb.aim.repository.ImageRepository;
 
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
 
 /**
@@ -29,6 +32,13 @@ public class ImageController {
     public String showImage(@PathVariable String imageId, Model model) {
         model.addAttribute("image_details",imageRepository.getImage(new Integer(imageId).intValue()));
         return "show-image";
+    }
+
+    @RequestMapping(value="/word_images/{wordId}",params={"status"})
+    public String wordImages(@PathVariable String wordId,
+                             @RequestParam("status") ImageStatus status, Model model) {
+        model.addAttribute("images",imageRepository.wordImages(new Integer(wordId),status));
+        return "list-images";
     }
 
 }
