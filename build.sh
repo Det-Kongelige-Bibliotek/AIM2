9eee77d3a6ddccfe980dd3f165a00894efe34676
jara <jara@kb.dk>
2019-08-07 14:32:17 +0200

    Improved build script

--- build.sh
+++ build.sh
@@ -2,27 +2,29 @@
 # source this from your .bashrc or .zshrc
 
 WEBAPPS="/var/lib/tomcat9/webapps"
-VERSION="2.0.4-SNAPSHOT" # match this with pom.xml
+VERSION="2.0.5-SNAPSHOT" # match this with pom.xml
 DIR=`dirname "$0"` # folder of this script
 
-# copy frontend without building
+# copy frontend without building, even works for jsp files
 function aimfront() {
   (
     cd "${DIR}/src/main/resources/static"
     gulp sass
   )
-  sudo cp -R "${DIR}/src/main/resources/static" "${WEBAPPS}/aim/WEB-INF/classes/static/"
+  sudo cp -R "${DIR}/src/main/webapp/WEB-INF" "${WEBAPPS}/aim/"
+  sudo cp -R "${DIR}/src/main/resources/static" "${WEBAPPS}/aim/WEB-INF/classes/"
   sudo touch "${WEBAPPS}/aim/WEB-INF/web.xml"
 }
 
 # build backend
 function aimback() {
   # clean
+  sudo rm -rf "${WEBAPPS}/aim/"
   sudo rm "${WEBAPPS}/aim.war"
-  sleep 5
   (
     cd "${DIR}"
-    mvn clean install -DskipTests=true
+    mvn install -DskipTests=true
     sudo cp "target/aim-${VERSION}.war" "${WEBAPPS}/aim.war"
+    sudo systemctl restart tomcat9
   )
 }
